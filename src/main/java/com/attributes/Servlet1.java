ce9cf07fe951b2d59fd222377778c3793cb12b4a
ParitoshDadhich <pdadhich0481@gmail.com>
2023-11-07 03:28:54 +0530

    implement sum & product by using attributes & parameter concept

---
+++ src/main/java/com/attributes/Servlet1.java
@@ -0,0 +1,58 @@
+package com.attributes;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int n1 = Integer.parseInt(request.getParameter("n1"));
+		int n2 = Integer.parseInt(request.getParameter("n2"));
+		
+		int sum = n1 + n2;
+		
+		// setting attribute
+		request.setAttribute("sum", sum);
+		
+		// now forwarding the request using RequestDispacter
+		RequestDispatcher rd = request.getRequestDispatcher("servlet2");
+		rd.forward(request, response);
+	}
+	
+
+}
---
+++ src/main/java/com/attributes/Servlet2.java
@@ -0,0 +1,65 @@
+package com.attributes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		int n1 = Integer.parseInt(request.getParameter("n1"));
+		int n2 = Integer.parseInt(request.getParameter("n2"));
+		
+		int product = n1 * n2;
+		
+		// setting attribute
+		request.setAttribute("product", product);
+		
+		// getting attribute from request object...
+		int sum = (int)request.getAttribute("sum");
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>");
+		writer.println("Sum is: " + sum);
+		writer.println("Prduct is: " + product);
+		writer.println("</h1>");
+		
+	}
+	
+
+}
