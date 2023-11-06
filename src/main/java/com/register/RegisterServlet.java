c2c5cc8a9755f5f4d39672bfc8832a8f489c44ac
ParitoshDadhich <pdadhich0481@gmail.com>
2023-11-07 00:45:31 +0530

    introduced RequestDispacter functionality

--- src/main/java/com/register/RegisterServlet.java
+++ src/main/java/com/register/RegisterServlet.java
@@ -3,6 +3,7 @@ package com.register;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,6 @@ public class RegisterServlet extends HttpServlet	 {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		 resp.setContentType("text/html");
 		 PrintWriter writer = resp.getWriter();
-		 System.out.println("Hello");
 		 writer.println("<h1>Welcome to Servlet Registeration Page!</h1>");
 		 
 		 String name = req.getParameter("user_name");
@@ -31,8 +31,30 @@ public class RegisterServlet extends HttpServlet	 {
 			 writer.println("<h2> Email : " + email + "<h2>");
 			 writer.println("<h2> Gender : " + gender + "<h2>");
 			 writer.println("<h2> Courses : " + course + "<h2>");
+			 
+			 
+			 
+			 
+			 /*
+			  * Assuming I have saved the above data to db;
+			  * calling success servlet from current survlet. 
+			  * Here I am sending the response of current Servlet to success Servlet;
+			  */
+			 
+			 RequestDispatcher rd = req.getRequestDispatcher("success");
+			 rd.forward(req, resp);
+			 
 		 } else {
 			 writer.println("<h2>You have not accepted terms and condtions!</h2>");
+			 
+			 // including output of index.html (home page)
+			 // Here RequestDispacter will be including the response of index.html with RequestServlet
+			 
+			 // getting object of requestDisptacher
+			 RequestDispatcher rd = req.getRequestDispatcher("index.html");
+			 
+			 // applying including logic
+			 rd.include(req, resp);
 		 }
 	}
 }
